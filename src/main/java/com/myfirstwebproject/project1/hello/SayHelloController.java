d2e40910b5da7406d4e7367ee9b34412a338cbdd
sagar6502 <ssunar@bayonesolutions.com>
2023-12-12 16:00:36 +0530

    Pushed the first application

---
+++ src/main/java/com/myfirstwebproject/project1/hello/SayHelloController.java
@@ -0,0 +1,10 @@
+package com.myfirstwebproject.project1.hello;
+
+public class SayHelloController {
+
+		//"say-hello" => "Hello! What are you learning today?"
+	
+	public String sayHello() {
+		return "Hello! What are you learning today?";
+	}
+}
